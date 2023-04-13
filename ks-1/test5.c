2bd298f16fc799fc9cfdce6a9ccf0b8bc162578c
chenxy <chenxy@chenxy.com>
2023-04-13 21:01:09 +0800

    增加比较函数内容

--- ks-1/test5.c
+++ ks-1/test5.c
@@ -14,8 +14,10 @@ struct Filenode{
     char fullname[256];     //文件、文件夹的全路径名称 比如： rootdir/dir1/dia2/folder
     char name[256];     //文件、文件夹的名称 比如： folder
     char result[24];    //比较的结果
+    char new_flag;      //是否是第二个比较目录的
     struct Filenode* child;     //子目录的指针
     struct Filenode* next;      //同目录的下一个文件夹、文件的指针
+    
 
 };
 
@@ -156,20 +158,84 @@ void PreOderTraverse(struct Filenode *szDir,int flag)
         traver_cnt--;
  } 
 
+void compareFolderNode(struct Filenode *szDir1,struct Filenode *szDir2)
+{
+
+}
+
+
 //比较两个目录szDir1，szDir2，以szDir1作为base, 比较结果保持到rslDir
  void compareTraverse(struct Filenode *szDir1,struct Filenode *szDir2,struct Filenode *rsltDir)
  {
-    if(szDir1 == NULL && szDir2 == NULL){
+    if(szDir1 == NULL || szDir2 == NULL){
         return;
     }
 
-    if(szDir1 == NULL && szDir2 != NULL){
-        
-    }else if(szDir1 != NULL && szDir2 == NULL){
-        
-    }else if(szDir1 != NULL && szDir2 != NULL){
+    //取第二个目录的第一个文件作为比较源，与第一个目录下的所有文件相比较
+    struct Filenode * compSource  = szDir2->child;
+    struct Filenode * compTarget  = szDir1->child;
+    if(compSource == NULL){
+        //如果第二个目录的内容为空
+    
+        if(compTarget != NULL){
+            //如果第一个目录的内容不为空
+            //创建一个新的文件节点，内容和第一个目录的child节点相同
+            struct Filenode *newDir = (struct Filenode *) calloc(sizeof(struct Filenode),1);
+            memcpy(newDir,compTarget,sizeof(struct Filenode));
+            //串到rsltDir中
+            rsltDir->child = newDir;
+        }else{
+            //两个目录都为空，啥都不做
 
+        }
+    }else{
+        //如果第二个目录的内容非空
+
+        if(compTarget == NULL){
+            //如果第一个目录为空 
+            //创建一个新的文件节点，内容和第二个目录的child节点相同，但是要标记一下是新增节点
+            struct Filenode *newDir = (struct Filenode *) calloc(sizeof(struct Filenode),1);
+            memcpy(newDir,compSource,sizeof(struct Filenode));
+            //但是要标记一下是新增节点
+            newDir->new_flag = 1;
+
+            //串到rsltDir中
+            rsltDir->child = newDir;
+        }else{
+
+            //如果第二个目录的内容非空，第一个目录也非空 开始循环比较
+            while(compSource != NULL){
+            {
+                //如果第一个目录非空，开始循环比较
+                while(compTarget != NULL){
+                    
+                    //文件、文件夹名称相同，且类型相同时，是可以深入比较的，否则就不同
+                    if(strcmp(compTarget->name,compSource->name) && (compTarget->type == compSource->type))
+                    {
+                        //创建一个新的文件节点，将其窜到szDir->child节点或者next节点上
+                        struct Filenode *newDir = (struct Filenode *) calloc(sizeof(struct Filenode),1);
+                        memcpy(newDir,compTarget,sizeof(struct Filenode));
+                        rsltDir->child = rsltDir;
+                        compareTraverse(compTarget,compSource,rsltDir);
+
+                        break;
+                    }
+                    compTarget = compTarget->next;
+                }
+                
+                //取第二个目录的下一个文件
+                compSource = compSource->next;
+            }
+        }
+
+
+
+            
+
+        }
     }
+
+
 
  }
 
